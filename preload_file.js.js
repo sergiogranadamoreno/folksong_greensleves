654fe333b7845ba4f6bf880c8361f11e537efebf
Sergio Granada-Moreno <sergiogranadamoreno@gmail.com>
2019-02-13 21:45:54 -0500

    Add files via upload

---
+++ preload_file.js.js
@@ -0,0 +1,28 @@
+//########################################
+//# This page contains score data, timing data and the media file path. Save it as a text file in
+//# the same folder as abcweb.html. Abcweb preloads score and media when it is opened with the
+//# file name as parameter in the url, for example: http://your.domain.org/abcweb.html?file_name
+//# Also works locally with file:///path/to/abcweb.html?file_name
+//# **** You have to correct the path to the media file below! (media_file="...";) ****
+//########################################
+//#
+media_file = "Greensleeves.mp3";
+offset_js = 0.00;
+opt = {"jump":0,"no_menu":0,"repufld":0,"noplyr":0,"nocsr":0,"media_height":"","btns":1,"ipadr":"","mstr":0,"autscl":0,"ctrmed":0,"ctrnot":0,"lncsr":0,"opacity":0.2,"synbox":0,"speed":1,"top_margin":0,"yubvid":"","nomed":0,"delay":0,"repskip":0,"spdctl":0,"lopctl":false,"metro":0,"btime":-1,"etime":0};
+lpRec = {"loopBtn":1,"loopStart":0,"loopEnd":24.25};
+times_arr = [[0.00,0.25,3.05,5.75,8.25,10.75,13.25,16.05,18.55,21.45],
+[21.45,23.95,26.45,28.95,31.45,33.95,36.65,39.15,41.65]];
+abc_arr = ["X:1",
+"T:Greensleeves",
+"%%scale 0.83",
+"L:1/8",
+"M:6/8",
+"I:linebreak $",
+"K:D",
+"V:1 treble-8 nm=\"Classical Guitar\" snm=\"Guit.\"",
+"V:1",
+" B | d2 e f>gf | e2 c A>Bc | d2 B B>^AB | c2 ^A F2 B | d2 e f>gf | e2 c A>Bc |$ d>cB ^A>^GA | %8",
+" B3 B3 | a3 a>^gf | e2 c A>Bc | d2 B B>^AB | c2 ^A F3 | a3 a>^gf |$ e2 c A>Bc | d>cB ^A>^GA | %16",
+" B3 B3 |] %17",
+""];
+abc_enc = [];
